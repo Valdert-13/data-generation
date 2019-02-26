4bcbf23f444d26787c851820b55383f94e4c379a
kirill <kirill.rum@gmail.com>
2019-02-26 17:27:46 +0300

    +

--- world_population.py
+++ world_population.py
@@ -1,4 +1,6 @@
 import json
+import pygal
+from pygal.style import RotateStyle
 
 from country_code import get_country_code
 
@@ -6,13 +8,33 @@ falename ='population_data.json'
 with open (falename) as f:
     pop_data = json.load (f)
 
+
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print (code + ': ' + str(population))
-        else:
-            print('ERROR - ' + country_name)
+            cc_populations[code] = population
+
+
+
+cc_pops_1, cc_pops_2, cc_pops_3 ={}, {}, {}
+
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+wm_style=RotateStyle('#336699')
+wm = pygal.maps.world.World(style = wm_style)
+wm.title ='World Population in 2010, by Countri'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+print (cc_pops_3)
+wm.render_to_file('world_population.svg')
 
